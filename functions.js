661cf49f8cb29733f4642ac18c0239d8d4a661d9
Ulaize <ulaize@creative-tweed.com>
2015-09-15 02:53:14 +0000

    Order array of strings by legnth

--- functions.js
+++ functions.js
@@ -1,1 +1,17 @@
 //This is the new file called functions.js
+
+//This orders an array by length on a decending order.
+
+
+function sortLongestString(a,b){
+   
+   return b.length - a.length;
+}
+
+var yourArray = ["Allo","Bonjour", "Comment ca va?"];
+
+yourArray.sort(sortLongestString);
+
+
+
+
